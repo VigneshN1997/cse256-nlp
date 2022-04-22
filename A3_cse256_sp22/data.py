6dae8a8dedc757b1d7bb54a045010827572eb870
VigneshN1997 <vigneshnandakumar1997@gmail.com>
2022-04-21 17:22:53 -0700

    implemented trigram model with laplace smoothing

--- A3_cse256_sp22/data.py
+++ A3_cse256_sp22/data.py
@@ -124,6 +124,27 @@ def learn_unigram(data, verbose=True):
         print("sample 2: ", " ".join(str(x) for x in sampler.sample_sentence([])))
     return unigram
 
+def learn_trigram(data, verbose=True):
+    """Learns a unigram model from data.train.
+
+    It also evaluates the model on data.dev and data.test, along with generating
+    some sample sentences from the model.
+    """
+    from lm import TrigramLaplace
+    trigram = TrigramLaplace()
+    trigram.fit_corpus(data.train)
+    if verbose:
+        print("vocab:", len(trigram.vocab()))
+        # evaluate on train, test, and dev
+        print("train:", trigram.perplexity(data.train))
+        print("dev  :", trigram.perplexity(data.dev))
+        print("test :", trigram.perplexity(data.test))
+        from generator import Sampler
+        sampler = Sampler(trigram)
+        print("sample 1: ", " ".join(str(x) for x in sampler.sample_sentence([])))
+        print("sample 2: ", " ".join(str(x) for x in sampler.sample_sentence([])))
+    return trigram
+
 def print_table(table, row_names, col_names, latex_file = None):
     """Pretty prints the table given the table, and row and col names.
 
@@ -156,7 +177,8 @@ if __name__ == "__main__":
         print(dname)
         data = read_texts("data/corpora.tar.gz", dname)
         datas.append(data)
-        model = learn_unigram(data)
+        model = learn_trigram(data)
+        # model = learn_unigram(data)
         models.append(model)
     # compute the perplexity of all pairs
     n = len(dnames)
--- A3_cse256_sp22/lm.py
+++ A3_cse256_sp22/lm.py
@@ -7,7 +7,10 @@ from __future__ import absolute_import
 import collections
 from math import log
 import sys
+import numpy as np
+import random
 import copy
+
 
 # Python 3 backwards compatibility tricks
 if sys.version_info.major > 2:
@@ -92,50 +95,143 @@ class Unigram(LangModel):
     def vocab(self):
         return self.model.keys()
 
-
-class Trigram(LangModel):
-    def __init__(self, backoff = 0.000001):
-        self.model = dict()
-        self.lbackoff = log(backoff, 2)
-        self.vocabulary = dict()
+class TrigramLaplace(LangModel):
+    def __init__(self):
         self.n = 3
+        self.unk_cutoff = 2.0
+        self.rare_words = dict()
+        self.rare_words_sum = 0
+        self.ngram_models = dict()
+        for i in range(self.n):
+            self.ngram_models[i] = dict()
+    
+    # creating unigram
+    def inc_word_unigram(self, w):
+        if w in self.ngram_models[0]:
+            self.ngram_models[0][w] += 1.0
+        else:
+            self.ngram_models[0][w] = 1.0
+
+    # fitting sentence for unigram
+    def fit_sentence_unigram(self, sentence):
+        for w in sentence:
+            self.inc_word_unigram(w)
+        self.inc_word_unigram('END_OF_SENTENCE')
+
+    def get_rare_words(self):
+        for w in self.ngram_models[0].keys():
+            if self.ngram_models[0][w] <= self.unk_cutoff:
+                rand_remove_prob = random.uniform(0, 1)
+                if rand_remove_prob >= 0.5:
+                    self.rare_words[w] = True
+                    self.rare_words_sum += self.ngram_models[0][w]
+        
+    
+    def remove_rare_set_unknown(self):
+        for w in self.rare_words:
+            self.ngram_models[0].pop(w, None)
+        self.ngram_models[0]["UNK"] = self.rare_words_sum
 
     def inc_ngram_count(self, words):
-        rec_model = self.model
-        for i in range(self.n):
-            if i == self.n - 1:
-                self.vocabulary[words[i]] = True
-                if words[i] in rec_model:
-                    rec_model[words[i]] += 1.0
-                else:
-                    rec_model[words[i]] = 1.0
-            else:
-                if words[i] not in rec_model:
-                    rec_model[words[i]] = dict()
-                rec_model = rec_model[words[i]]
-                
+        word_2 = words[0]
+        if word_2 in self.rare_words:
+            word_2 = "UNK"
+        word_1 = words[1]
+        if word_1 in self.rare_words:
+            word_1 = "UNK"
+        word_0 = words[2]
+        if word_0 in self.rare_words:
+            word_0 = "UNK"
+        
+        if word_1 not in self.ngram_models[1]:
+            self.ngram_models[1][word_1] = dict()
+        
+        if word_0 not in self.ngram_models[1][word_1]:
+            self.ngram_models[1][word_1][word_0] = 0.0
+
+        self.ngram_models[1][word_1][word_0] += 1.0
+
+        if word_2 not in self.ngram_models[2]:
+            self.ngram_models[2][word_2] = dict()
+
+        if word_1 not in self.ngram_models[2][word_2]:
+            self.ngram_models[2][word_2][word_1] = dict()
+        
+        if word_0 not in self.ngram_models[2][word_2][word_1]:
+            self.ngram_models[2][word_2][word_1][word_0] = 0.0
+
+        self.ngram_models[2][word_2][word_1][word_0] += 1.0
+
     def fit_sentence(self, sentence):
         num_words = len(sentence)
         modified_sentence = ["*"]*(self.n - 1)
         modified_sentence.extend(sentence)
         modified_sentence.append('END_OF_SENTENCE')
+        if "*" not in self.ngram_models[1]:
+            self.ngram_models[1]["*"] = dict()
+        if "*" not in self.ngram_models[1]["*"]:
+            self.ngram_models[1]["*"]["*"] = 0.0
+
+        self.ngram_models[1]["*"]["*"] += 1.0
         for i in range(num_words + 1):
             self.inc_ngram_count(modified_sentence[i:i+self.n])
 
+    def fit_corpus(self, corpus):
+        """Learn the language model for the whole corpus.
+
+        The corpus consists of a list of sentences."""
+        for s in corpus:
+            self.fit_sentence_unigram(s)
+            
+        self.get_rare_words()
+        self.remove_rare_set_unknown()
+        for s in corpus:
+            self.fit_sentence(s)
+
+        self.norm()
+
     def norm(self):
         """Normalize and convert to log2-probs."""
-        tot = 0.0
-        for word in self.model:
-            tot += self.model[word]
-        ltot = log(tot, 2)
-        for word in self.model:
-            self.model[word] = log(self.model[word], 2) - ltot
+        self.ngram_models_counts = copy.deepcopy(self.ngram_models)
+        vocab_size = len(self.ngram_models[0])
+
+        for w_2 in self.ngram_models[2]:
+            for w_1 in self.ngram_models[2][w_2]:
+                bigram_count = self.ngram_models[1][w_2][w_1]
+                laplace_tot = bigram_count + vocab_size
+                log_tot_k = log(laplace_tot, 2)
+                 
+                for w_0 in self.ngram_models[2][w_2][w_1]:
+                    self.ngram_models[2][w_2][w_1][w_0] = log(self.ngram_models[2][w_2][w_1][w_0] + 1, 2) - log_tot_k
+                
+                if "UNK" not in self.ngram_models[2][w_2][w_1]:
+                    num_words_present_in_bigram = len(self.ngram_models[2][w_2][w_1])
+                    num_unk = vocab_size - num_words_present_in_bigram
+                    self.ngram_models[2][w_2][w_1]["UNK"] = log(num_unk, 2) - log_tot_k
 
     def cond_logprob(self, word, previous):
-        if word in self.model:
-            return self.model[word]
+        context_present = True
+        prvs_context = []
+        if len(previous) < self.n - 1:
+            prvs_context.extend(["*"]*(self.n - 1 - len(previous)))
+        prvs_context.extend(previous[-(self.n - 1):])
+        
+        mod = self.ngram_models[2]
+        for prev in prvs_context:
+            if prev not in mod:
+                context_present = False
+                break
+            else:
+                mod = mod[prev]  
+        if context_present:
+            if word in mod:
+                return mod[word]
+            else:
+                return mod["UNK"]
         else:
-            return self.lbackoff
+            return -log(float(len(self.ngram_models[0])), 2)
 
     def vocab(self):
-        return self.vocabulary.keys()
+        return self.ngram_models[0].keys()
+    
+    
