0c5277e115a89e2e954cdcb37fe40360554c6024
VigneshN1997 <vigneshnandakumar1997@gmail.com>
2022-05-14 22:42:37 -0700

    completed implementation of ibm 1 and 2

--- A5_cse256_sp22/ibm_models.py
+++ A5_cse256_sp22/ibm_models.py
@@ -11,7 +11,7 @@ class IBMModel1():
         self.calc_num_diff_words()
         self.init_translation_probs()
         self.params_file = "translation.params"
-        
+
     def read_corpus_addnull(self):
         "Reads a corpus and adds in the NULL word."
         self.english = [["*"] + e_sent.split() for e_sent in open(self.eng_corpus)]
@@ -19,10 +19,7 @@ class IBMModel1():
         self.n_sentences = len(self.english)
         # print(len(self.english))
 
-    def num_sentences(self):
-        # print(self.english[1000])
-        return len(self.english)
-    
+
     def calc_num_diff_words(self):
         self.n_e = {}
         wordpairs = set()
@@ -41,6 +38,7 @@ class IBMModel1():
     def init_translation_probs(self):
         # print("here")
         self.translation_probs = {} # t(f | e)
+        self.deltas = {}
         for k in range(self.n_sentences):
             eng_sent = self.english[k]
             for_sent = self.foreign[k]
@@ -56,15 +54,15 @@ class IBMModel1():
                 if l not in self.counts_align_f[i]:
                     self.counts_align_f[i][l] = {}
                 if m not in self.counts_align_f[i][l]:
-                    self.counts_align_f[i][l][m] = 0.0
+                    self.counts_align_f[i][l][m] = 0
                 for j in range(l):
                     e = eng_sent[j]
-                    self.translation_probs[f][e] = 1.0 / float(self.n_e[e])
+                    self.translation_probs[f][e] = 1 / self.n_e[e]
                     # print(f, e, str(self.translation_probs[f][e]))
                     if e not in self.counts_e_f:
                         self.counts_e_f[e] = {}
-                    self.counts_e_f[e][f] = 0.0
-                    self.counts_e[e] = 0.0
+                    self.counts_e_f[e][f] = 0
+                    self.counts_e[e] = 0
                     if j not in self.counts_align:
                         self.counts_align[j] = {}
                     if i not in self.counts_align[j]:
@@ -72,7 +70,13 @@ class IBMModel1():
                     if l not in self.counts_align[j][i]:
                         self.counts_align[j][i][l] = {}
                     if m not in self.counts_align[j][i][l]:
-                        self.counts_align[j][i][l][m] = 0.0
+                        self.counts_align[j][i][l][m] = 0
+
+                    if k not in self.deltas:
+                        self.deltas[k] = {}
+                    if i not in self.deltas[k]:
+                        self.deltas[k][i] = {}
+                    self.deltas[k][i][j] = 0
 
     def find_new_translations(self):
         for k in range(self.n_sentences):
@@ -94,12 +98,12 @@ class IBMModel1():
             m = len(for_sent)
             for i in range(m):
                 f = for_sent[i]
-                self.counts_align_f[i][l][m] = 0.0
+                self.counts_align_f[i][l][m] = 0
                 for j in range(l):
                     e = eng_sent[j]
-                    self.counts_e_f[e][f] = 0.0
-                    self.counts_e[e] = 0.0
-                    self.counts_align[j][i][l][m] = 0.0
+                    self.counts_e_f[e][f] = 0
+                    self.counts_e[e] = 0
+                    self.counts_align[j][i][l][m] = 0
 
     def write_params_to_file(self):
         with open(self.params_file, "w") as pf:
@@ -128,12 +132,12 @@ class IBMModel1():
                 nwords_for = len(for_sent)
                 l = nwords_eng
                 m = nwords_for
-                for i in range(nwords_for):
-                    norm_den = 0.0
+                for i in range(m):
+                    norm_den = 0
                     f = for_sent[i]
-                    for j in range(nwords_eng):
+                    for j in range(l):
                         norm_den += self.translation_probs[f][eng_sent[j]]
-                    for j in range(nwords_eng):
+                    for j in range(l):
                         e = eng_sent[j]
                         delta_k_i_j = self.translation_probs[f][e] / norm_den
                         self.counts_e_f[e][f] += delta_k_i_j
@@ -157,7 +161,7 @@ class IBMModel1():
         m = len(for_sent)
         for i in range(m):
             max_prob = 0.0
-            max_prob_index = 0
+            max_prob_index = -1
             f = for_sent[i]
             a = [sentence_num, 0, i + 1]
             if f in self.translation_probs:
@@ -165,10 +169,17 @@ class IBMModel1():
                     e = eng_sent[j]
                     if e not in self.translation_probs[f]:
                         continue
-                    if self.translation_probs[f][e] >= max_prob:
+                    # if sentence_num == 2 and i+1==3:
+                    #     print(j,f,e,self.translation_probs[f][e])
+
+                    if self.translation_probs[f][e] > max_prob:
                         max_prob = self.translation_probs[f][e]
                         max_prob_index = j
                 a[1] = max_prob_index
+            else:
+                continue
+            if max_prob_index == -1:
+                continue
             alignments.append(str(a[0]) + " " + str(a[1]) + " " + str(a[2]) + "\n")
         return alignments
         
@@ -177,7 +188,7 @@ class IBMModel1():
         dev_eng = [["*"] + e_sent.split() for e_sent in open(dev_eng_corpus)]
         dev_for = [f_sent.split() for f_sent in open(dev_for_corpus)]
         parallel_dev_corpus = zip(dev_eng, dev_for)
-        self.read_params_from_file()
+        # self.read_params_from_file()
         sentence_num = 1
         all_alignments = []
         for eng_sent, for_sent in parallel_dev_corpus:
@@ -190,8 +201,8 @@ class IBMModel1():
 class IBMModel2(IBMModel1):
     def __init__(self, english_corpus, foreign_corpus):
         super().__init__(english_corpus, foreign_corpus)
-        self.init_soft_alignment_params()
         self.read_params_from_file()
+        self.init_soft_alignment_params()
     
     def init_soft_alignment_params(self):
         self.q_params = {}
@@ -209,7 +220,7 @@ class IBMModel2(IBMModel1):
                     if l not in self.q_params[j][i]:
                         self.q_params[j][i][l] = {}
                     if m not in self.q_params[j][i][l]:
-                        self.q_params[j][i][l][m] = 1.0/float(l)
+                        self.q_params[j][i][l][m] = 1.0/float(l + 1)
     
     def find_new_translations2(self):
         for k in range(self.n_sentences):
@@ -224,6 +235,7 @@ class IBMModel2(IBMModel1):
                     self.translation_probs[f][e] = self.counts_e_f[e][f] / self.counts_e[e]
                     self.q_params[j][i][l][m] = self.counts_align[j][i][l][m] / self.counts_align_f[i][l][m]
 
+
     def estimate_params(self, n_iterations):
         n_sentences = len(self.english)
         for s in range(n_iterations):
@@ -236,12 +248,12 @@ class IBMModel2(IBMModel1):
                 nwords_for = len(for_sent)
                 l = nwords_eng
                 m = nwords_for
-                for i in range(nwords_for):
+                for i in range(m):
                     norm_den = 0.0
                     f = for_sent[i]
-                    for j in range(nwords_eng):
+                    for j in range(l):
                         norm_den +=  (self.q_params[j][i][l][m] * self.translation_probs[f][eng_sent[j]])
-                    for j in range(nwords_eng):
+                    for j in range(l):
                         e = eng_sent[j]
                         delta_k_i_j = (self.q_params[j][i][l][m] * self.translation_probs[f][e]) / norm_den
                         self.counts_e_f[e][f] += delta_k_i_j
@@ -277,8 +289,8 @@ class IBMModel2(IBMModel1):
                     if m not in self.q_params[j][i][l]:
                         continue
                     val = self.q_params[j][i][l][m] * self.translation_probs[f][e]
-                    if val >= max_prob:
-                        max_prob = self.translation_probs[f][e]
+                    if val > max_prob:
+                        max_prob = val
                         max_prob_index = j
                 if max_prob_index == -1:
                     continue
@@ -287,22 +299,6 @@ class IBMModel2(IBMModel1):
                 continue
             alignments.append(str(a[0]) + " " + str(a[1]) + " " + str(a[2]) + "\n")
         return alignments
-    
-    def find_alignments(self, dev_eng_corpus, dev_for_corpus, out_key_file):
-        print("calling model 2 alignments")
-        dev_eng = [["*"] + e_sent.split() for e_sent in open(dev_eng_corpus)]
-        dev_for = [f_sent.split() for f_sent in open(dev_for_corpus)]
-        parallel_dev_corpus = zip(dev_eng, dev_for)
-        # self.read_params_from_file()
-        sentence_num = 1
-        all_alignments = []
-        for eng_sent, for_sent in parallel_dev_corpus:
-            alignments = self.find_alignment_sentence(eng_sent, for_sent, sentence_num)
-            all_alignments.extend(alignments)
-            sentence_num += 1
-        with open(out_key_file, "w")as of:
-            of.writelines(all_alignments)
-            
 
 
 english_corpus = "corpus.en"
